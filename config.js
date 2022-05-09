5c55a4fb4d101952c219762e106e1d12bc51f6b8
Omar Basaqer <o.basaqer@mobily.com.sa>
2022-05-09 16:02:07 +0300

    Moved urls to admin

--- config.js
+++ config.js
@@ -2,9 +2,4 @@
 exports.title = "Dynamic Mock server";
 
 // The API list of urls to call one by one, in order. When failing, the next url will be called.
-exports.urls = [
-  "https://jsonplaceholder.typicode.co123/todos",
-  "https://jsonplaceholder.typicode.co123/todos",
-  "https://jsonplaceholder.typicode.com",
-  "http://10.14.193.77:9010",
-];
+// Moved URLS to (/admin) page
