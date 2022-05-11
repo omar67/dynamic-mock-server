8d253690f5ef33e5c7276e4707d9a095cf7b2d08
Omar Basaqer <o.basaqer@mobily.com.sa>
2022-05-11 13:08:49 +0300

    Pass paths to admin page

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,16 @@
 var express = require("express");
 const { title } = require("../config");
+const { getAllPaths } = require("../data/data_access/pathDA");
 const { getAllUrls } = require("../data/data_access/urlDA");
 var router = express.Router();
 
 /* GET home page. */
 router.get("/", async function (req, res, next) {
-  res.render("admin", { title: title, urls: await getAllUrls() });
+  res.render("admin", {
+    title: title,
+    urls: await getAllUrls(),
+    paths: await getAllPaths(),
+  });
 });
 
 module.exports = router;
