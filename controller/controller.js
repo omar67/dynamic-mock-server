6f5b6c04d8248fb7f4409f361cf590fb7956f997
Omar Basaqer <o.basaqer@mobily.com.sa>
2022-05-11 14:12:20 +0300

    Refactored code, and finalized implementation

--- controller/controller.js
+++ controller/controller.js
@@ -1,47 +1,77 @@
 const { default: axios } = require("axios");
-const { accepts } = require("express/lib/request");
 const createError = require("http-errors");
-const { urls } = require("../config");
+const { getAllPaths, getPath } = require("../data/data_access/pathDA");
 const { getAllUrls } = require("../data/data_access/urlDA");
+var qs = require("qs");
+const FormData = require("form-data");
+const MyGenericError = require("../data/errors/MyGenricError");
+const { getRequestConfig } = require("./axiosConfig");
+
 var axiosService = axios;
 
 async function handleMockServer(req, res, next) {
-  axiosService = axios.create({
-    headers: {
-      post: req.headers,
-    },
-  });
-  var response = "";
-  if (req.method == "GET") response = await getRequestHelper(req);
-  else if (req.method == "POST") response = await postRequestHelper(req);
-  else if (req.method == "PATCH") response = await patchRequestHelper(req);
-  else createError("Method not allowed");
+  axiosService = axios.create({});
 
-  return res.json(response);
+  try {
+    var response = await requestHandler(req);
+    return res.send(response);
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function requestHandler(req) {
+  let urls = await getUrls();
+  let path = await findPath(req.originalUrl, req.method);
+  var response = "";
+
+  if (req.method == "GET") response = await getRequestHelper(req, urls, path);
+  else if (req.method == "POST")
+    response = await postRequestHelper(req, urls, path);
+  else if (req.method == "PATCH")
+    response = await patchRequestHelper(req, urls, path);
+  else throw new MyGenericError("Method not allowed", 405);
+  return response;
 }
 // -- GET --
 /**
  *  Handles the GET request and return the response.
  * @returns {Object}
  */
-async function getRequestHelper(req) {
-  console.log("Url :", (await getUrls())[0]);
-  return await getRequest(req, (await getUrls())[0], 0);
+async function getRequestHelper(req, urls, path) {
+  if (path) return await getRequest(req, getPathUrl(urls, path), 0);
+
+  return await getRequest(req, urls, 0);
 }
 
 /**
+ * Find path object from path name
+ * @param {String} path - Path to be searched
+ * @returns {Path} returns the path object from the path
+ */
+async function findPath(path, method) {
+  return await getPath(path, method);
+}
+
+function createURLsError() {
+  throw new MyGenericError("Sorry, All urls failed from MockServer", 404);
+}
+/**
  * Recursion method to call GET api from the list of urls.
  */
-async function getRequest(req, url, index) {
-  if (!url) return createError("Sorry, All urls failed");
+async function getRequest(req, urls, index) {
+  if (urls.length === index) return createURLsError();
 
   try {
-    let response = await axiosService.get(url + req.originalUrl);
+    let response = await axiosService({
+      url: urls[index] + req.originalUrl,
+      ...getRequestConfig(req, "get"),
+    });
     if (isSuccess(response.status)) return response?.data ?? undefined;
-    else return await getRequest(req, (await getUrls())[index], index + 1);
+    else return await getRequest(req, urls, index + 1);
   } catch (e) {
-    console.error("Exception thrown:", e);
-    return await getRequest(req, (await getUrls())[index], index + 1);
+    // console.error("Exception thrown:", e);
+    return await getRequest(req, urls, index + 1);
   }
 }
 
@@ -50,22 +80,28 @@ async function getRequest(req, url, index) {
  *  Handles the POST request and return the response.
  * @returns {Object}
  */
-async function postRequestHelper(req) {
-  return await postRequest(req, (await getUrls())[0], 0);
+async function postRequestHelper(req, urls, path) {
+  if (path) return await postRequest(req, getPathUrl(urls, path), 0);
+
+  return await postRequest(req, urls, 0);
 }
 
 /**
  * Recursion method to call POST api from the list of urls.
  */
-async function postRequest(req, url, index) {
-  if (!url) return createError("Sorry, All urls failed");
+async function postRequest(req, urls, index) {
+  if (urls.length === index) return createURLsError();
   try {
-    let response = await axiosService.post(url + req.originalUrl, req.body);
+    let response = await axiosService({
+      url: urls[index] + req.originalUrl,
+      ...getRequestConfig(req, "post"),
+    });
+
     if (isSuccess(response.status)) return response?.data ?? undefined;
-    else return await postRequest(req, (await getUrls())[index], index + 1);
+    else return await postRequest(req, urls, index + 1);
   } catch (e) {
     console.error("Exception thrown:", e);
-    return await postRequest(req, (await getUrls())[index], index + 1);
+    return await postRequest(req, urls, index + 1);
   }
 }
 
@@ -74,23 +110,29 @@ async function postRequest(req, url, index) {
  *  Handles the PATCH request and return the response.
  * @returns {Object}
  */
-async function patchRequestHelper(req) {
-  return await patchRequest(req, (await getUrls())[0], 0);
+async function patchRequestHelper(req, urls, path) {
+  if (path) return await patchRequest(req, getPathUrl(urls, path), 0);
+
+  return await patchRequest(req, urls, 0);
 }
 
 /**
  * Recursion method to call PATCH api from the list of urls.
  */
-async function patchRequest(req, url, index) {
-  if (!url) return createError("Sorry, All urls failed");
+async function patchRequest(req, urls, index) {
+  if (urls.length === index) return createURLsError();
 
   try {
-    let response = await axiosService.patch(url + req.originalUrl, req.body);
+    let response = await axiosService({
+      url: urls[index] + req.originalUrl,
+      ...getRequestConfig(req, "patch"),
+    });
     if (isSuccess(response.status)) return response?.data ?? undefined;
-    else return await patchRequest(req, (await getUrls())[index], index + 1);
+    else return await patchRequest(req, urls, index + 1);
   } catch (e) {
-    console.error("Exception thrown:", e);
-    return await patchRequest(req, (await getUrls())[index], index + 1);
+    console.log("request api", req.headers);
+    // console.error("Exception thrown:", e.response);
+    return await patchRequest(req, urls, index + 1);
   }
 }
 
@@ -104,10 +146,25 @@ function isSuccess(code) {
 
 /**
  * Return the list of urls
- * @returns {Array<String>} List of Urls
+ * @returns {Promise<Array<String>>} List of Urls
  */
 async function getUrls() {
-  return await (await getAllUrls()).map((data) => data.host);
+  return (await getAllUrls()).map((data) => data.host);
+}
+
+/**
+ * Return the list of paths
+ * @returns {Array<String>} List of Urls
+ */
+async function getPaths() {
+  return (await getAllPaths()).map((data) => data.path);
+}
+
+// Returns the list of urls that matches the path
+function getPathUrl(urls, path) {
+  return urls.filter((url) => {
+    return url == path.host;
+  });
 }
 module.exports = {
   handleMockServer,
